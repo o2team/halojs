aaf71b6cc21fb19a11af69e34add1ed095c7bd48
leeenx <lee200822@126.com>
2016-01-12 15:55:11 +0800

    add download folder

---
+++ source/download/dev/objectUpdate.js
@@ -0,0 +1,96 @@
+/*
+	@ author leeenx
+    @ version 1.0.0
+    @ data: 2015-12-07
+    @ 作用: object/array类型对象的更新
+    @ 用法： 
+    $.objectUpdate(//对象更新
+		{
+			obj1: obj1,
+			obj2: obj2,
+			incremental: 1//是否增量更新，默认为1。如果为0，当前方法相当于obj1是obj2的深度拷贝
+		}
+    );
+    //简单的用法：
+    $.objectUpdate(obj1,obj2[,incremental]);
+*/
+define("objectUpdate",function(require,exports,module){
+	'use zeptojs';
+	var getType=function(obj){
+		return Object.prototype.toString.call(obj);
+	},
+	isObject=function(obj){
+		return "[object Object]"==Object.prototype.toString.call(obj);
+	},
+	isArray=function(obj){
+		return "[object Array]"==Object.prototype.toString.call(obj);
+	},
+	incremental=1,
+	copyObj=function(obj1,obj2,incremental){
+		for(var i in obj2){
+			if(obj1[i]!=obj2[i]){
+				//如果类型不对，obj1需要强制转换成obj2
+				isObject(obj2[i])
+				?
+				(isObject(obj1[i])||(obj1[i]={}),copyObj(obj1[i],obj2[i]))
+				:
+				(
+					isArray(obj2[i])
+					?
+					(isArray(obj1[i])||(obj1[i]=[]),copyArr(obj1[i],obj2[i]))
+					:
+					(/*即不是object也不是array*/
+						obj1[i]=obj2[i]
+					)
+				);
+			}
+		}
+		if(incremental)return ;//如果是增量更新，在此中断
+		for(var i in obj1){
+			if(typeof(obj2[i])=="undefined"){
+				delete obj1[i];//删除obj2中不存在的成员
+			}
+		}
+	},
+	copyArr=function(arr1,arr2,incremental){
+		for(var i=0,len=arr2.length;i<len;++i){
+			if(arr1[i]!=arr2[i]){
+				//如果类型不对，arr1需要强制转换成arr2
+				isObject(arr2[i])
+				?
+				(isObject(arr1[i])||(arr1[i]={}),copyObj(arr1[i],arr2[i]))
+				:
+				(
+					isArray(arr2[i])
+					?
+					(isArray(arr1[i])||(arr1[i]=[]),copyArr(arr1[i],arr2[i]))
+					:
+					(/*即不是object也不是array*/
+						arr1[i]=arr2[i]
+					)
+				);
+			}
+		}
+		if(incremental)return ;//如果是增量更新，在此中断
+		if(arr1.length>arr2.length){
+			//需要删除多余的长度
+			arr1.splice(arr2.length,arr1.length);
+		}
+	};
+	module.exports=function(arg){
+		var argLen=arguments.length;
+		if(argLen<=0)return ;
+		argLen>=2&&(arg={obj1:arguments[0],obj2:arguments[1],incremental: typeof(arguments[2])=="undefined"?incremental:arguments[2]});
+		arg="[object Object]"!=getType(arg)?{}:arg;//保证为object类型
+		var type1=getType(arg.obj1),type2=getType(arg.obj2);
+		if("[object Object]"!=type1&&"[object Array]"!=type1){
+			throw("objectUpdate fail! Invalid type!");
+			return ;
+		}
+		if(type1!=type2){
+			throw("objectUpdate fail! Can't update different type");
+			return ;
+		}
+		"[object Object]"==type1?copyObj(arg.obj1,arg.obj2,arg.incremental):copyArr(arg.obj1,arg.obj2,arg.incremental);
+	};
+});
---
+++ source/download/dev/randomSort.js
@@ -0,0 +1,29 @@
+/*
+    @ author leeenx
+    @ version 1.0.0
+    @ data: 2015-12-07
+    @ 将数据随机排序
+    @ 用法：
+    @ 1. $.randomSort([...]);
+    @ 2. $.randomSort([...],n);//返回指定长度的数组
+*/
+
+define("randomSort",function(require,exports,module){
+	'use zeptojs';
+	return function(arr){
+		//将队列随机打乱
+		var arrLen=arr.length;
+		if(arrLen<=0)return arr;
+		arrLen=parseInt(arrLen)||0;
+		var index_arr=[],new_arr=[],len=arr.length,index_arr_len=len;
+		if(arrLen<=0)arrLen=len;
+		for(var i=0;i<len;++i)index_arr[i]=i;//索引数组
+		while(index_arr_len>0&&arrLen){
+			var _rnd=parseInt(Math.random()*index_arr_len);
+			new_arr[len-index_arr_len]=arr[index_arr.splice(_rnd,1)];
+			--index_arr_len;
+			--arrLen;
+		}
+		return new_arr;//生成新数组
+	}
+});
