f8f64bc14b811e3a5463e797c32fcc528ed90ed6
Littly <544028951@qq.com>
2016-01-21 14:52:06 +0800

    tweaks

--- internal.js
+++ internal.js
@@ -26,15 +26,16 @@ hexo.init().then(function () {
 	hexo.extend.console.register('deploy', 'deploy your website', {
 
 	}, function (args) {
-		hexo.call(argv._[0], {
-			_: 'g'
-		}).then(function () {
-			deployer.deploy(hexo);
-		}).catch(function () {
-			hexo.call('help', {
-				_: 'h'
+		if (args.g) {
+			hexo.call(argv._[0], {
+				_: 'g'
+			}).then(function () {
+				deployer.deploy(hexo);
 			});
-		});
+		} else {
+			deployer.deploy(hexo);
+		}
+
 	});
 	hexo.call(argv._[0], argv)
 		.catch(function () {
