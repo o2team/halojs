3c0b9bf43961e97ca34f11a9ad3f70ef17b5fe99
Littly <544028951@qq.com>
2016-01-21 14:18:40 +0800

    Site updated

--- external.js
+++ external.js
@@ -1,20 +1,35 @@
 var Hexo = require('hexo'),
-	minimist = require('minimist');
+	deployer = require('./lib/deployer'),
+	minimist = require('minimist'),
+	util = require('util'),
+	generator = require('./lib/generator');
 
 var hexo,
 	argv;
+
+console.lookin = function (obj) {
+	console.log(util.inspect(obj, {
+		showHidden: true,
+		depth: null,
+		colors: true,
+		customInspect: false
+	}));
+};
 
 argv = minimist(process.argv.slice(2))
 hexo = new Hexo(process.cwd(), {
 	config: 'config/_external.yml'
 });
 
-
-
-hexo.init().then(function(){
+hexo.init().then(function () {
 	console.log('environment: external');
+	hexo.extend.console.register('deploy', 'deploy your website', {
+
+	}, function (args) {
+		deployer.deploy(hexo);
+	});
 	hexo.call(argv._[0], argv)
 		.catch(function () {
-
+			hexo.call('help', argv);
 		});
 });
